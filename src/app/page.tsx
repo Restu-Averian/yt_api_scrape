d8205d5c8bf077cf3d635574e87d31881d9a9a20
Restu-Averian <restuaverianputra123@gmail.com>
2023-12-31 19:55:54 +0700

    revert add config cors and updt home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-import { useMemo } from "react";
-
 export default function Page() {
-  const originUrl = window ? window.location.origin : null;
-  return <a href={`${originUrl}/api`}>Api Link</a>;
+  // const originUrl = window ? window.location.origin : null;
+  return <></>;
 }
